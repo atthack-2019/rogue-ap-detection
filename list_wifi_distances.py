f08bd39088e19051c3190cf0984116eaccf52fbd
adamivora <xivora@fi.muni.cz>
2019-11-22 19:19:48 +0100

    filter wifi distances to 2.4 GHz

--- list_wifi_distances.py
+++ list_wifi_distances.py
@@ -6,10 +6,15 @@ from wifi import Cell
 def get_freq(frequency):
     return float(frequency.split()[0]) * 1000
 
+def is_2_4(freq):
+    return freq > 2000 and freq < 3000
+
 def get_networks():
     nets = list(Cell.all('wlan0'))
     for net in nets:
-        print(net.address, dist(net.signal, get_freq(net.frequency)))
+        freq = get_freq(net.frequency)
+        if is_2_4(freq):
+            print(net.address, dist(net.signal, freq))
 
 def dist(sig, freq=2412):
     return 10**((27.55-(20*np.log10(freq)) - sig)/20.0)
